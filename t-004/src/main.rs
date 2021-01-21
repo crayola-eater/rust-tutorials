85a4106eeca6f0ca0b8a675bab5159b8cfdcb09c
crayola-eater <user@domain.com>
2021-01-21 20:07:32 +0000

    Done #4

---
+++ t-004/src/main.rs
@@ -0,0 +1,8 @@
+// https://www.youtube.com/watch?v=gMtxQwwvU88&list=PLVvjrrRCBy2JSHf9tGxGKJ-bYAN_uDCUL&index=4
+fn main() {
+    let mut x = 45;
+    println!("The value of x is {}", x);
+    
+    x = 60;
+    println!("The value of x is {}", x);
+}
