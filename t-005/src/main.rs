bb5393aa59655354da52c78f91b14cea815f8a17
crayola-eater <user@domain.com>
2021-01-21 20:18:07 +0000

    Done #5

---
+++ t-005/src/main.rs
@@ -0,0 +1,9 @@
+// https://www.youtube.com/watch?v=RBo8Vcbpc4o&list=PLVvjrrRCBy2JSHf9tGxGKJ-bYAN_uDCUL&index=5
+fn main() {
+    // By default, i is i32 here.
+    let i = 45;
+    let f = 6.7;
+    let b: bool = false;
+
+    println!("Values are {}, {}, {}", i, f, b)
+}
