4e64aa522c8ec8e1400bcac87fc94b50e2fef9f1
crayola-eater <user@domain.com>
2021-01-21 20:00:21 +0000

    Done #3

---
+++ t-003/src/main.rs
@@ -0,0 +1,5 @@
+// https://www.youtube.com/watch?v=Qbz3gKCt-w0&list=PLVvjrrRCBy2JSHf9tGxGKJ-bYAN_uDCUL&index=3
+fn main() {
+    println!("Hello, world!");
+    println!("We can write single line comments with // and multi-line comments with /* */.")
+}
