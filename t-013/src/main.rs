67b6d222667042a66c2d9680e4c4b0685d8c5380
crayola-eater <a-sheikh@outlook.com>
2021-01-24 12:11:29 +0000

    Done #13

---
+++ t-013/src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    print_numbers_to(20);
+}
+
+fn print_numbers_to(num: u32) {
+    for n in 1..num {
+        if is_even(n) {
+            println!("{} is even!", n);
+        }
+        else {
+            println!("{} is odd!", n)
+        }
+    }
+}
+
+fn is_even(num: u32) -> bool {
+    0 == num % 2
+}
