1e028d3812111933021d2f905861e873ea580107
crayola-eater <a-sheikh@outlook.com>
2021-01-26 17:25:05 +0000

    Done #28

---
+++ t-028/src/main.rs
@@ -0,0 +1,30 @@
+struct Person {
+    name: String,
+    age: u8,
+}
+
+trait HasVoiceBox {
+    fn speak(&self);
+
+    fn can_speak(&self) -> bool;
+}
+
+impl HasVoiceBox for Person {
+    fn speak(&self) {
+        println!("Hello my name is {}", self.name);
+    }
+
+    fn can_speak(&self) -> bool {
+        self.age > 0
+    }
+}
+
+fn main() {
+    let person = Person { name: String::from("Bob"), age: 41 };
+
+    println!("Can {} speak? {}", person.name, person.can_speak());
+    if person.can_speak() {
+        person.speak();
+    }
+
+}
